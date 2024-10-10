ea9dcd740aea82c2926e99ad527bd1764fb81860
Texas-bread <1758955335@qq.com>
2024-10-10 21:06:01 +0800

    新鲜好物业务

---
+++ src/apis/home.js
@@ -0,0 +1,18 @@
+import httpInstance from '@/utils/http.js'
+
+//获取banner
+export function getBannerAPI(){
+    return httpInstance({
+        url: 'home/banner',
+    })
+}
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => {
+    return httpInstance({
+        url:'/home/new'
+    })
+}
