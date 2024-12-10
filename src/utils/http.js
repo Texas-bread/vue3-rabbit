d5e96496e980ca8dbe1530e079da7680f796ef38
Texas-bread <1758955335@qq.com>
2024-12-10 14:35:18 +0800

    登录-表单校验-token拦截器中携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {useUserStore} from "@/stores/user.js";
 
 // 创建axios实例
 const http = axios.create({
@@ -9,6 +10,14 @@ const http = axios.create({
 // axios请求拦截器
 http.interceptors.request.use(config => {
     // You can modify the config here if needed
+    //1.从pinia获取token数据
+        const userStore = useUserStore()
+        //2.获取后端的要求拼接token数据
+        const token= userStore.userInfo.token
+
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
 }, e => Promise.reject(e));
 
